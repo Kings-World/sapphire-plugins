8dd2e7ef90786671f25fca9ff9e7fcd1e16205b9
Seren_Modz 21 <seren@kings-world.net>
2023-07-18 04:38:57 +0100

    chore: remove more code i forgot to remove
    i moved the job setter into the store with a try/catch
    
    loggers return void, so the returns are pretty useless
    
    remove the unload method becauuse the client can access the job themself
    
    hopefully this is the last of my mistakes 😭
    
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- packages/cron/src/lib/structures/CronTask.ts
+++ packages/cron/src/lib/structures/CronTask.ts
@@ -1,72 +1,53 @@
 import { Piece } from "@sapphire/pieces";
-import { CronJob } from "cron";
+import type { CronJob } from "cron";
 import type { CronTaskStore } from "./CronTaskStore";
 import type { CronJobOptions } from "../types/CronTaskTypes";
 import type { Awaitable } from "@sapphire/framework";
 
 export abstract class CronTask extends Piece {
-    job: CronJob;
-
+    declare job: CronJob;
     declare store: CronTaskStore;
     declare options: CronTask.Options;
 
     constructor(context: Piece.Context, options: CronTask.Options) {
         super(context, options);
-
-        this.job = new CronJob({
-            ...options,
-            start: false,
-            context: this,
-            onTick: this.run.bind(this),
-            timeZone: options.timeZone ?? this.container.cron.defaultTimezone,
-        });
     }
 
     abstract run(): Awaitable<unknown>;
 
     info(message: string, ...other: unknown[]) {
-        return this.container.logger.info(
+        this.container.logger.info(
             `CronTask[${this.name}] ${message}`,
             ...other
         );
     }
 
     error(message: string, ...other: unknown[]) {
-        return this.container.logger.error(
+        this.container.logger.error(
             `CronTask[${this.name}] ${message}`,
             ...other
         );
     }
 
     warn(message: string, ...other: unknown[]) {
-        return this.container.logger.warn(
+        this.container.logger.warn(
             `CronTask[${this.name}] ${message}`,
             ...other
         );
     }
 
     debug(message: string, ...other: unknown[]) {
-        return this.container.logger.debug(
+        this.container.logger.debug(
             `CronTask[${this.name}] ${message}`,
             ...other
         );
     }
 
     trace(message: string, ...other: unknown[]) {
-        return this.container.logger.trace(
+        this.container.logger.trace(
             `CronTask[${this.name}] ${message}`,
             ...other
         );
-    }
-
-    override unload() {
-        // job is undefined when an error occurrs during the CronJob construction
-        // it is not typed as optional because it should never be undefined otherwise
-        if (this.job?.running) {
-            this.job.stop();
-        }
-
-        return super.unload();
     }
 }
 
