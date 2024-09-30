47cb7ef6970ae6255fe068f2f5cec1ace1ed8109
Seren_Modz 21 <seren@kings-world.net>
2024-09-30 09:12:37 +0100

    feat: format luxon timezones to their tz identifier (#49)
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- packages/cron/src/lib/structures/CronTaskStore.ts
+++ packages/cron/src/lib/structures/CronTaskStore.ts
@@ -1,6 +1,7 @@
 import { Store } from '@sapphire/pieces';
 import { CronJob } from 'cron';
 import { CronTask } from './CronTask';
+import { Info, DateTime, FixedOffsetZone, type Zone } from 'luxon';
 
 export class CronTaskStore extends Store<CronTask, 'cron-tasks'> {
 	public constructor() {
@@ -48,7 +49,7 @@ export class CronTaskStore extends Store<CronTask, 'cron-tasks'> {
 				onTick: () => void value.run.bind(value)(),
 				start: false,
 				context: value,
-				timeZone: options.timeZone ?? defaultTimezone
+				timeZone: this.formatLuxonZone(options.timeZone ?? defaultTimezone)
 			});
 		} catch (error) {
 			value.error('Encountered an error while creating the cron job', error);
@@ -74,5 +75,25 @@ export class CronTaskStore extends Store<CronTask, 'cron-tasks'> {
 	public override clear() {
 		this.stopAll();
 		return super.clear();
+	}
+
+	/**
+	 * Formats a Luxon-compatible timezone string into a TZ timezone string.
+	 * This is required because Sentry requires time zones to be in TZ format.
+	 * @param timezone The Luxon-compatible timezone to format.
+	 * @returns The formatted TZ timezone.
+	 * @private
+	 */
+	private formatLuxonZone(timezone?: string | Zone | number) {
+		const zone = Info.normalizeZone(timezone);
+
+		// If the zone is invalid, return the system zone
+		if (!zone.isValid) return DateTime.local().zoneName;
+
+		// If the zone is a fixed offset zone, return the IANA name
+		if (zone instanceof FixedOffsetZone) return zone.ianaName;
+
+		// Otherwise, return the zone name
+		return zone.name;
 	}
 }
--- packages/cron/src/lib/types/CronTaskTypes.ts
+++ packages/cron/src/lib/types/CronTaskTypes.ts
@@ -20,3 +20,17 @@ export interface CronTaskHandlerOptions {
 }
 
 export type CronJobOptions = Omit<CronJobParams<null, CronTask>, 'onTick' | 'onComplete' | 'start' | 'context' | 'utcOffset'>;
+
+/**
+ * The @types/luxon package doesn't seem to be up-to-date with luxon.
+ * Therefore, I have to manually add the ianaName getter to the FixedOffsetZone class.
+ * The code can be found here: https://github.com/moment/luxon/blob/3e9983cd0680fdf7836fcee638d34e3edc682380/src/zones/fixedOffsetZone.js#L80C7-L80C15
+ */
+declare module 'luxon' {
+	interface FixedOffsetZone {
+		/**
+		 * The IANA name of this zone, i.e. `Etc/UTC` or `Etc/GMT+/-nn`
+		 */
+		get ianaName(): string;
+	}
+}
